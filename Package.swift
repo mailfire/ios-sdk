f09e13fc489197db24eba489d0dcdcd21cc03d0a
Aleksansdr <oleksandr.liashko@corp.mailfire.io>
2020-12-09 01:31:14 +0200

    xcframework test

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets  can depend on other targets in this package, and on products in packages this package depends on.
         //.binaryTarget(name: "Mailfire", path: "./Sources/Mailfire.xcframework")
-        .binaryTarget(name: "Mailfire", url: "https://github.com/mailfire/ios-sdk/blob/master/Mailfire.xcframework.zip", checksum: "640cb306ad15f8cd3d27b0d6428fc7561645000080bf07e8d0e168371cf1ab65")
+        .binaryTarget(name: "Mailfire", url: "https://github.com/mailfire/ios-sdk/blob/master/xcframework/Mailfire.xcframework.zip", checksum: "c18fcf5df59fc2e43f74204301966d7f85b6a60f37a6153c33095300c0d2d6e1")
     ]
 )
